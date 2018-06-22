fc928754cfdae34f79b0830de86e2cd72227f014
JaneLMF <long.meifeng@pivosgroup.com.cn>
2018-06-22 13:13:19 -0400

    * add trim asin and sku

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -906,7 +906,10 @@ class Picking(models.Model):
                     todo_moves |= move
                     # Assign move as it was assigned before
                     toassign_moves |= new_move
-                products.append({'product_id': move.product_id, 'move_index': index, 'asin': move.asin, 'sellerSKU': move.sku, 'condition': move.condition, 'quantity': int(move.product_uom_qty)})
+                if move.asin and move.sku:
+                    move.asin = move.asin.strip()
+                    move.sku = move.sku.strip()
+                    products.append({'product_id': move.product_id, 'move_index': index, 'asin': move.asin, 'sellerSKU': move.sku, 'condition': move.condition, 'quantity': int(move.product_uom_qty)})
             if products and len(products) > 0:
                 _logger.warning(products)
                 c_shipment = CreateInboundShipment()
